0969aa7d9ef10384a1c7f5e8e7c82b4df2afb634
sysnlbrsc <Scheepersbryan@gmail.com>
2020-05-18 17:20:04 +0200

    Added plugin

--- javaexample/javaexample.gradle.kts
+++ javaexample/javaexample.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.openosrsVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.2"
+version = "0.1.2"
 
 project.extra["PluginName"] = "Quest navigator" // This is the name that is used in the external plugin manager panel
 project.extra["PluginDescription"] = "Quest navigator plugin" // This is the description that is used in the external plugin manager panel
