f8ae40934a8e2b9d037b3ef218f571adf016e929
sysnlbrsc <Scheepersbryan@gmail.com>
2020-05-18 17:15:17 +0200

    Added plugin

--- javaexample/javaexample.gradle.kts
+++ javaexample/javaexample.gradle.kts
@@ -25,10 +25,10 @@ import ProjectVersions.openosrsVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.1"
+version = "0.0.2"
 
-project.extra["PluginName"] = "Java example plugin" // This is the name that is used in the external plugin manager panel
-project.extra["PluginDescription"] = "Java example plugin" // This is the description that is used in the external plugin manager panel
+project.extra["PluginName"] = "Quest navigator" // This is the name that is used in the external plugin manager panel
+project.extra["PluginDescription"] = "Quest navigator plugin" // This is the description that is used in the external plugin manager panel
 
 dependencies {
     annotationProcessor(Libraries.lombok)
